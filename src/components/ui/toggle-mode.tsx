00566aa9fd03b505886b93d9832e464ef6a99008
lorenzo <lorenzo.softdev@gmail.com>
2023-12-31 16:57:41 +0100

    add margin left to theme menu

--- src/components/ui/toggle-mode.tsx
+++ src/components/ui/toggle-mode.tsx
@@ -24,7 +24,7 @@ export function ModeToggle() {
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent align='end' className='ml-2'>
         <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
